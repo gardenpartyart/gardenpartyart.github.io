4c6afef06298f1a8d04214a02e98ec9cd167da94
gardenpartyart <honeybee8768@gmail.com>
2024-09-24 16:42:35 -0400

    Create scripts.js

---
+++ assets/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Lazy-load images
+    const lazyImages = document.querySelectorAll('img');
+    
+    lazyImages.forEach(img => {
+        img.add
