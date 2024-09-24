dd897bb46bd2879574c2e51af78e3623d008dfd8
gardenpartyart <honeybee8768@gmail.com>
2024-09-24 17:20:13 -0400

    Delete assets directory

--- assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Lazy-load images
-    const lazyImages = document.querySelectorAll('img');
-    
-    lazyImages.forEach(img => {
-        img.add
